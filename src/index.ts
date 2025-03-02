6a7841b724100f0b960617ccdcb53e4aa9fb16bd
Jack Sierkstra <jacksierkstra@gmail.com>
2025-03-02 11:46:11 +0100

    Added an index file so the code can be consumed.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Validator, ValidatorImpl } from "@lib/validator/validator";
+export { XMLParser } from "@lib/xml/parser";
+export { XSDParser } from "@lib/xsd/parser";
+export { ValidationResult } from "@lib/types/validation";
