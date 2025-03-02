3d8bb6f80030c6c18120e802e86e59aa0b465f43
Jack Sierkstra <jacksierkstra@gmail.com>
2025-03-02 19:37:10 +0100

    Exporting everything because it can be consumed that way.

--- src/core/main.ts
+++ src/core/main.ts
@@ -1,3 +1,22 @@
-class Checkr {
-    
+import { ValidationResult } from "@lib/types/validation";
+import { Validator, ValidatorImpl } from "@lib/validator/validator";
+import { XMLParser, XMLParserImpl } from "@lib/xml/parser";
+import { XSDParser, XSDParserImpl } from "@lib/xsd/parser";
+
+export class Checkr {
+
+    private xmlParser: XMLParser;
+    private xsdParser: XSDParser;
+    private validator: Validator;
+
+    constructor() {
+        this.xmlParser = new XMLParserImpl();
+        this.xsdParser = new XSDParserImpl(this.xmlParser);
+        this.validator = new ValidatorImpl(this.xmlParser, this.xsdParser);
+    }
+
+    public validate(xml: string, xsd: string): Promise<ValidationResult> {
+        return this.validator.validate(xml, xsd);
+    }
+
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { Validator, ValidatorImpl } from "@lib/validator/validator";
-export { XMLParser } from "@lib/xml/parser";
-export { XSDParser } from "@lib/xsd/parser";
+export { Checkr } from "@lib/core/main";
 export { ValidationResult } from "@lib/types/validation";
+export { Validator, ValidatorImpl } from "@lib/validator/validator";
+export { XMLParser, XMLParserImpl } from "@lib/xml/parser";
+export { XSDParser, XSDParserImpl } from "@lib/xsd/parser";
